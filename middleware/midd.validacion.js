2c167a056bff01bc04932f5865b3b00cbd6a337e
josueSoP <josue.soto.plaza@gmail.com>
2021-06-23 16:05:55 -0500

    validacion y seguridad

--- middleware/midd.validacion.js
+++ middleware/midd.validacion.js
@@ -12,14 +12,14 @@ module.exports = {
         usuario: Joi.string().alphanum().min(3).max(50).required(),
         email: Joi.string().email().max(50).required(),
         pass: Joi.string().regex(/^[a-zA-Z0-9]{6,16}$/).min(6).required(),
-        imagen: Joi.string().alphanum().min(3).max(100).required(),
-        ciudad: Joi.string().regex(/^[ .a-zA-Z]+$/).min(3).max(50).required(),
-        pais: Joi.string().regex(/^[ .a-zA-Z]+$/).min(3).max(50).required(),
-        edad: Joi.number().min(2).max(3).allow(null).required(),
-        estudios: Joi.string().regex(/^[ .a-zA-Z]+$/).min(10).max(100).required(),
-        linkedin: Joi.string().regex(/^[ .a-zA-Z]+$/).min(3).max(100).required(),
-        hobies: Joi.string().regex(/^[ .a-zA-Z]+$/).min(3).max(300).required(),
-        cv: Joi.string().alphanum().min(3).max(100).required()
+        imagen: Joi.string().min(3).max(100).required(),
+        ciudad: Joi.string().regex(/^[ .a-zA-Z0-9]{6,16}$/).min(3).max(50).required(),
+        pais: Joi.string().regex(/^[ .a-zA-Z0-9]{6,16}$/).min(3).max(50).required(),
+        edad: Joi.number().min(1).allow(null).required(),
+        estudios: Joi.string().regex(/^[ .a-zA-Z0-9]{6,16}$/).min(10).max(100).required(),
+        linkedin: Joi.string().min(3).max(100).required(),
+        hobies: Joi.string().regex(/^[ .a-zA-Z0-9]{6,16}$/).min(3).max(300).required(),
+        cv: Joi.string().min(3).max(100).required()
     }),
 
     modelActualiza: Joi.object().keys({
@@ -28,14 +28,14 @@ module.exports = {
         usuario: Joi.string().alphanum().min(3).max(50).required(),
         email: Joi.string().email().max(50).required(),
         pass: Joi.string().regex(/^[a-zA-Z0-9]{6,16}$/).min(6).required(),
-        imagen: Joi.string().alphanum().min(3).max(100).required(),
-        ciudad: Joi.string().regex(/^[ .a-zA-Z]+$/).min(3).max(50).required(),
-        pais: Joi.string().regex(/^[ .a-zA-Z]+$/).min(3).max(50).required(),
-        edad: Joi.number().min(2).max(3).allow(null).required(),
-        estudios: Joi.string().regex(/^[ .a-zA-Z]+$/).min(10).max(100).required(),
-        linkedin: Joi.string().regex(/^[ .a-zA-Z]+$/).min(3).max(100).required(),
-        hobies: Joi.string().regex(/^[ .a-zA-Z]+$/).min(3).max(300).required(),
-        cv: Joi.string().alphanum().min(3).max(100).required()
+        imagen: Joi.string().min(3).max(100).required(),
+        ciudad: Joi.string().regex(/^[ .a-zA-Z0-9]{6,16}$/).min(3).max(50).required(),
+        pais: Joi.string().regex(/^[ .a-zA-Z0-9]{6,16}$/).min(3).max(50).required(),
+        edad: Joi.number().min(1).allow(null).required(),
+        estudios: Joi.string().regex(/^[ .a-zA-Z0-9]{6,16}$/).min(10).max(100).required(),
+        linkedin: Joi.string().min(3).max(100).required(),
+        hobies: Joi.string().regex(/^[ .a-zA-Z0-9]{6,16}$/).min(3).max(300).required(),
+        cv: Joi.string().min(3).max(100).required()
     }),
 
 }
